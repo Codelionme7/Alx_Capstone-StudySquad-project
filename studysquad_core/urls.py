cc52aa74a2f76a5cf4284d472530d3814ff7438d
Codelionme7 <mugishalionel02@gmail.com>
2026-02-20 23:46:22 +0300

    Update urls.py

--- studysquad_core/urls.py
+++ studysquad_core/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')), # Route user requests to users app
+    path('api/users/', include('users.urls')),
+    path('api/academics/', include('academics.urls')),   
+    path('api/scheduling/', include('scheduling.urls')),
 ]
