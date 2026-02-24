4c456c36cc15a8f8f0aeb137ed94341b3dd14238
Codelionme7 <mugishalionel02@gmail.com>
2026-02-25 00:30:00 +0300

    Update urls.py

--- studysquad_core/urls.py
+++ studysquad_core/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # StudySquad Endpoints
     path('api/users/', include('users.urls')),
-    path('api/academics/', include('academics.urls')),   
+    path('api/academics/', include('academics.urls')),
     path('api/scheduling/', include('scheduling.urls')),
+    path('api/groups/', include('groups.urls')), 
+    
+    # Auto-Generated API Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
